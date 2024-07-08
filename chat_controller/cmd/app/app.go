7f2b6827ac2760c05a6ecb1ee3636927959071ce
hoo <tbnsok40@gmail.com>
2024-07-09 01:40:17 +0900

    feat: app 실행 시 service, repository 연동

--- chat_controller/cmd/app/app.go
+++ chat_controller/cmd/app/app.go
@@ -3,20 +3,26 @@ package app
 import (
 	"chat_controller/config"
 	"chat_controller/network"
+	"chat_controller/repository"
 	"chat_controller/service"
 )
 
 type App struct {
-	config  *config.Config
-	service *service.Service
-	network *network.Server
+	config     *config.Config
+	service    *service.Service
+	network    *network.Server
+	repository *repository.Repository
 }
 
 func NewApp(config *config.Config) *App {
 	a := &App{config: config}
-	// netWork 에 대해 세팅해줘야함.
-
-	a.network = network.NewNetwork(a.service, config.Info.Port)
+	var err error
+	if a.repository, err = repository.NewRepository(config); err != nil {
+		panic(err)
+	} else {
+		a.service = service.NewService(a.repository)
+		a.network = network.NewNetwork(a.service, config.Info.Port)
+	}
 	return a
 }
 
