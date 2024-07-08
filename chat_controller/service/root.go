69e13039bf9445a74099f589a3e758498e25d401
hoo <tbnsok40@gmail.com>
2024-07-09 01:39:30 +0900

    feat: service setup

--- chat_controller/service/root.go
+++ chat_controller/service/root.go
@@ -1,10 +1,19 @@
 package service
 
+import "chat_controller/repository"
+
 type Server struct {
 }
 
 type Service struct {
+	repository    *repository.Repository
 	AvgServerList map[string]bool
+}
+
+// (s *Service) 얘는 왜 receiver 를 안갖지?
+func NewService(repository *repository.Repository) *Service {
+	s := &Service{repository: repository, AvgServerList: make(map[string]bool)}
+	return s
 }
 
 func (s *Service) GetAvgServerList() []string {
