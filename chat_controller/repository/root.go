a855107f3cd7c96056806b60aee9a99707aabdd7
hoo <tbnsok40@gmail.com>
2024-07-09 18:07:21 +0900

    feat: fetch available server lists

--- chat_controller/repository/root.go
+++ chat_controller/repository/root.go
@@ -2,14 +2,20 @@ package repository
 
 import (
 	"chat_controller/config"
+	"chat_controller/types/table"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
 )
 
 type Repository struct {
 	cfg *config.Config
 	db  *sql.DB
 }
+
+const (
+	serverInfo = "chatting.server_info"
+)
 
 func NewRepository(cfg *config.Config) (*Repository, error) {
 
@@ -22,4 +28,33 @@ func NewRepository(cfg *config.Config) (*Repository, error) {
 		return nil, err
 	}
 	return r, nil
+}
+
+func (r *Repository) GetAvailableServerList() ([]*table.ServerInfo, error) {
+	qs := query([]string{"SELECT * FROM", serverInfo, "WHERE available = 1"})
+	if cursor, err := r.db.Query(qs); err != nil {
+		panic(err)
+	} else {
+		defer cursor.Close()
+
+		var result []*table.ServerInfo
+		for cursor.Next() {
+			d := new(table.ServerInfo)
+			if err = cursor.Scan(&d.IP, &d.Available); err != nil {
+				panic(err)
+			} else {
+				result = append(result, d)
+			}
+		}
+		if len(result) == 0 {
+			return []*table.ServerInfo{}, nil
+		} else {
+			return result, nil
+		}
+	}
+
+}
+
+func query(qs []string) string {
+	return strings.Join(qs, " ") + ";"
 }
--- chat_controller/service/root.go
+++ chat_controller/service/root.go
@@ -1,6 +1,9 @@
 package service
 
-import "chat_controller/repository"
+import (
+	"chat_controller/repository"
+	"chat_controller/types/table"
+)
 
 type Server struct {
 }
@@ -13,10 +16,11 @@ type Service struct {
 // (s *Service) 얘는 왜 receiver 를 안갖지?
 func NewService(repository *repository.Repository) *Service {
 	s := &Service{repository: repository, AvgServerList: make(map[string]bool)}
+	s.setServerInfo()
 	return s
 }
 
-func (s *Service) GetAvgServerList() []string {
+func (s *Service) ResponseLiveServerList() []string {
 	var res []string
 	for ip, available := range s.AvgServerList {
 		if available == true {
@@ -24,4 +28,19 @@ func (s *Service) GetAvgServerList() []string {
 		}
 	}
 	return res
+}
+
+func (s *Service) setServerInfo() {
+	if serverList, err := s.GetAvailableServerList(); err != nil {
+		panic(err)
+	} else {
+		for _, server := range serverList {
+			s.AvgServerList[server.IP] = true
+		}
+	}
+
+}
+
+func (s *Service) GetAvailableServerList() ([]*table.ServerInfo, error) {
+	return s.repository.GetAvailableServerList()
 }
