9bac0aef659fd570ba16a16122e998fad363f604
hoo <tbnsok40@gmail.com>
2024-07-11 16:03:50 +0900

    fix: controller server 정리

--- chat_controller/network/root.go
+++ chat_controller/network/root.go
@@ -3,6 +3,9 @@ package network
 import (
 	"chat_controller/service"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"log"
 	"net/http"
 )
@@ -19,10 +22,24 @@ type tower struct {
 
 func NewNetwork(service *service.Service, port string) *Server {
 	s := &Server{engine: fiber.New(), port: port}
-	//s.engine.Use(fiber.Logger()) // fiber logger 찾아야함
-	//s.engine.Config(fiber.Recovery) // fiber recovery 찾아야함
+	// Logger middleware
+	s.engine.Use(logger.New())
+
+	// Recover middleware
+	s.engine.Use(recover.New())
+
+	// CORS middleware
+	s.engine.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,PUT,DELETE,PATCH",
+		AllowHeaders:     "Origin,Content-Length,Content-Type,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Authorization,X-Requested-With,Expires",
+		ExposeHeaders:    "Origin,Content-Length,Content-Type,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Authorization,X-Requested-With,Expires",
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	t := &tower{server: s}
+	t.server.engine.Get("/ping", t.healthCheck)
 	t.server.engine.Get("/server-list", t.serverList) // 요 표현식 이해안됨, t.serverList() 도 아니고 그냥 t.serverList ?
 	return s
 }
@@ -31,10 +48,13 @@ func (t *tower) serverList(ctx *fiber.Ctx) error {
 	return response(ctx, http.StatusOK, t.server.service.ResponseLiveServerList())
 }
 
+func (t *tower) healthCheck(ctx *fiber.Ctx) error {
+	return ctx.SendString("헬스체크 준비 할 완료~")
+}
+
 func (s *Server) Start() error {
-	log.Println("Start Controller Server")
-	app := fiber.New()
-	err := app.Listen(s.port)
+	log.Println("Start Controller Server on port", s.port)
+	err := s.engine.Listen(s.port)
 	if err != nil {
 		panic(err)
 	} else {
