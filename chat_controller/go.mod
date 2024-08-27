313c4ab2bcf81f1d7c92d4b3224b0118df05dad1
hoo <tbnsok40@gmail.com>
2024-08-27 14:51:22 +0900

    feat: goroutine load test

--- chat_controller/go.mod
+++ chat_controller/go.mod
@@ -11,8 +11,9 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -20,7 +21,7 @@ require (
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/fasthttp v1.52.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 )
--- chat_controller/network/root.go
+++ chat_controller/network/root.go
@@ -2,12 +2,19 @@ package network
 
 import (
 	"chat_controller/service"
+	"encoding/json"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gorilla/websocket"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type Server struct {
@@ -41,6 +48,7 @@ func NewNetwork(service *service.Service, port string) *Server {
 	t := &tower{server: s}
 	t.server.engine.Get("/ping", t.healthCheck)
 	t.server.engine.Get("/server-list", t.serverList) // 요 표현식 이해안됨, t.serverList() 도 아니고 그냥 t.serverList ?
+	t.server.engine.Get("/load-test", t.loadTest)
 	return s
 }
 
@@ -59,5 +67,127 @@ func (s *Server) Start() error {
 		panic(err)
 	} else {
 		return nil
+	}
+}
+
+type message struct {
+	Name    string    `json:"name"`
+	Message string    `json:"message"`
+	Room    string    `json:"room"`
+	When    time.Time `json:"when"`
+}
+
+type LoadTestConfig struct {
+	Connections int    `json:"connections"`
+	Duration    int    `json:"duration"`
+	ServerURL   string `json:"server_url"`
+}
+
+func (t *tower) loadTest(ctx *fiber.Ctx) error {
+	config := new(LoadTestConfig)
+	if err := ctx.BodyParser(config); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+	if config.Connections <= 0 || config.Duration <= 0 || config.ServerURL == "" {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid configuration"})
+	}
+	var wg sync.WaitGroup
+	results := make(chan string, config.Connections)
+	for i := 0; i < config.Connections; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			err := runWebSocketClient(config.ServerURL, config.Duration, id, results, ctx)
+			if err != nil {
+				results <- fmt.Sprintf("Client %d error: %v", id, err)
+			}
+		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	var response []string
+	for result := range results {
+		response = append(response, result)
+	}
+
+	return ctx.JSON(fiber.Map{"results": response})
+}
+
+func runWebSocketClient(serverURL string, duration, id int, results chan<- string, ctx *fiber.Ctx) error {
+	// Connect to the WebSocket server
+	header := http.Header{
+		"Cookie": []string{"auth=" + ctx.Cookies("auth")},
+	}
+	c, resp, err := websocket.DefaultDialer.Dial(serverURL, header)
+	if err != nil {
+		if resp != nil {
+			log.Printf("Client %d received HTTP response: %d %s", id, resp.StatusCode, resp.Status)
+			body, _ := ioutil.ReadAll(resp.Body)
+			log.Printf("Response body: %s", string(body))
+		}
+		return fmt.Errorf("dial error: %v", err)
+	}
+	defer c.Close()
+
+	messagesSent := 0
+	messagesReceived := 0
+
+	// Set up done channel for test duration
+	done := make(chan bool)
+	go func() {
+		time.Sleep(time.Duration(duration) * time.Second)
+		done <- true
+	}()
+
+	// Set up message sending goroutine
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				msg := message{
+					Name:    fmt.Sprintf("user_%d", id),
+					Message: fmt.Sprintf("Test message %d", rand.Intn(1000)),
+					Room:    "Load Test Room",
+					When:    time.Now(),
+				}
+
+				msgBytes, err := json.Marshal(msg)
+				if err != nil {
+					log.Printf("Client %d error marshaling message: %v", id, err)
+					continue
+				}
+
+				if err := c.WriteMessage(websocket.TextMessage, msgBytes); err != nil {
+					log.Printf("Client %d error writing message: %v", id, err)
+					return
+				}
+				messagesSent++
+				time.Sleep(time.Millisecond * 100) // Adjust this delay as needed
+			}
+		}
+	}()
+
+	// Main loop for receiving messages
+	for {
+		select {
+		case <-done:
+			results <- fmt.Sprintf("Client %d completed. Sent: %d, Received: %d", id, messagesSent, messagesReceived)
+			return nil
+		default:
+			_, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Printf("Client %d read error: %v", id, err)
+				results <- fmt.Sprintf("Client %d error: %v", id, err)
+				return err
+			}
+			log.Printf("Client %d received: %s", id, string(msg))
+			messagesReceived++
+		}
 	}
 }
